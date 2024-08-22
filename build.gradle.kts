f9ebdd8f0d45edc99508de5d11a6df84aa20018c
Ilia Skliaruk <58298423+skliaruk@users.noreply.github.com>
2024-08-22 13:28:14 +0300

    Add application plugin and configure main class in build.gradle.kts
    - Applied the `application` plugin to enable the `run` task.
    - Set the `mainClass` property to `com.skliaruk.MainKt`.
    - Configured the `jar` task to include the main class in the manifest.
    - Ensured `standardInput` is set for the `run` task.
    - Updated dependencies and plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.0"
     kotlin("plugin.serialization").version("2.0.0")
+    application
 }
 
 group = "com.skliaruk"
@@ -20,6 +21,13 @@ dependencies {
     implementation("com.github.ajalt.clikt:clikt:4.0.0") // for CLI parsing
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0") // for date handling
     implementation("io.ktor:ktor-server-auth:2.3.0")
+}
+application {
+    mainClass.set("com.skliaruk.MainKt")
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
 
 tasks.jar {
